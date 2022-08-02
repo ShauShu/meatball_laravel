5141c973430a1bccb8d1f96a859f9207e0c229bd
shu <jacky21601@gmail.com>
2022-08-02 14:46:00 +0800

    many to many commodity_user

--- app/Models/Commodity.php
+++ app/Models/Commodity.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Commodity extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'price',
+        'type',
+        'info',
+        'pic',
+    ];
+
+    public function users(){
+        return $this->belongsToMany('App\Models\User');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,9 @@ class User extends Authenticatable
     public function orders(){
         return $this->hasMany('App\Models\Order');
     }
+    public function commodities(){
+        return $this->belongsToMany('App\Models\Commodity');
+    }
 
 
     /**
